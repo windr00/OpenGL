ea0c6d71418e5e9f919611cb60609da92ad36f16
Sophie <xiaowenyn@126.com>
2016-07-22 17:08:02 +0800

    working on EventScheduler

---
+++ OpenGL/GlobalEventSchedule.cpp
@@ -0,0 +1,9 @@
+//
+//  GlobalEventSchedule.cpp
+//  OpenGL
+//
+//  Created by Sophie Xiong on 16/7/22.
+//  Copyright © 2016年 冉惟之. All rights reserved.
+//
+
+#include "GlobalEventSchedule.hpp"
---
+++ OpenGL/GlobalEventSchedule.hpp
@@ -0,0 +1,21 @@
+//
+//  GlobalEventSchedule.hpp
+//  OpenGL
+//
+//  Created by Sophie Xiong on 16/7/22.
+//  Copyright © 2016年 冉惟之. All rights reserved.
+//
+
+#ifndef GlobalEventSchedule_hpp
+#define GlobalEventSchedule_hpp
+
+#include <stdio.h>
+
+class GlobalEventSchedule {
+    
+    public :
+    
+    
+};
+
+#endif /* GlobalEventSchedule_hpp */
